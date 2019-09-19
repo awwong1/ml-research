e8af6e102f512a8f65bd5db8c082abefb1fbbde1
Alexander Wong <alex@udia.ca>
2019-09-19 09:08:35 -0600

    manual seed bool functionality

--- util/seed.py
+++ util/seed.py
@@ -4,9 +4,15 @@ import random
 
 def set_seed(seed, logger=None):
     """Set python random seed, pytorch manual seed, and enable deterministic CUDNN
+    seed: True, None, int
+    - if None (or falsy), do not set a seed
+    - if True, set a manual seed using torch.seed()
+    - if type(seed) is int, set manual seed
     """
-    if seed is None:
+    if not seed:
         return
+    if seed is True:
+        seed = torch.seed()
     random.seed(seed)
     torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
