f0c63f243e5c67acf3a593703dc714bc793c9997
Alexander Wong <alex@udia.ca>
2019-10-21 12:59:59 -0600

    wrong truncate script

--- scripts/truncate_train_multi_adaptive_cifar100_vgg19_bn.sh
+++ scripts/truncate_train_multi_adaptive_cifar100_vgg19_bn.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# Train ten different models for cifar100 vgg19 with batch norm
 
-NUM_RUNS=9
-
-for RUN_ID in `seq 0 ${NUM_RUNS}`;
+for RUN_ID in `seq 9 -1 1`;
 do
-    OVERRIDE="{\"gpu_ids\":[0],\"exp_name\":\"adaptive_constrained-cifar100-vgg19_bn-run${RUN_ID}\"}"
+    OVERRIDE="{\"gpu_ids\":[1],\"exp_name\":\"truncate_train-adaptive_constrained-cifar100-vgg19_bn-run0-${RUN_ID}\",\"truncate_train_data_ratio\":0.${RUN_ID}}"
     echo "$OVERRIDE"
     python3 main.py configs/cifar100/adaptive_constrained_vgg19_bn.json \
         --override ${OVERRIDE}
